356a74e81351c7529608999c322fc808b02eb1f6
Diego Cunado <diegocunado@gmail.com>
2017-05-12 16:02:05 +0100

    Add pagination to views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,8 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
 
-  def index
-
-    @articles = Article.all
+  def index  
+    @articles = Article.paginate(page: params[:page], per_page: 10, ).order('id DESC')
 
   end
 
